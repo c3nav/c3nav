b79f84f0b637e7a6de916c513da81658bec765c9
Laura Klünder <laura@codingcatgirl.de>
2016-08-19 19:45:11 +0200

    change sources list format and add editor base + sources overlays

--- src/c3nav/mapdata/views.py
+++ src/c3nav/mapdata/views.py
@@ -1,3 +1,19 @@
+import mimetypes
+
+from django.contrib.admin.views.decorators import staff_member_required
+from django.http import Http404, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from ..mapdata import mapmanager
+
+
+@staff_member_required
+def source(request, filename):
+    source = mapmanager.sources_by_filename.get(filename)
+    if source is None:
+        raise Http404('Source does not exist')
+
+    response = HttpResponse(content_type=mimetypes.guess_type(source.src)[0])
+    with open(source.src, 'rb') as f:
+        response.write(f.read())
+    return response
