e37f6c371098c3e68ae54ac6838eed26bf8d6cf1
Nicole Klünder <nomoketo@nomoketo.de>
2016-08-19 13:51:09 +0200

    size, sources, levels

--- src/c3nav/mapdata/classes.py
+++ src/c3nav/mapdata/classes.py
@@ -11,6 +11,7 @@ class MapManager:
     def __init__(self):
         self.main_pkg = None
         self.pkgs = OrderedDict()
+        self.levels = []
 
     def add_map_dir(self, path):
         pkg = MapDataPackage(path)
@@ -22,12 +23,18 @@ class MapManager:
                 raise MapInitError('There can not be more than one root map package: tried to add '+pkg.name+', '
                                    'but '+self.main_pkg.name+' was there first.')
             self.main_pkg = pkg
+            self.levels = pkg.levels
+            self.size = pkg.size
         else:
             if pkg.extends not in self.pkgs:
                 raise MapInitError('map package'+pkg.name+' extends '+pkg.exends+', which was not imported '
                                    'beforehand.')
 
         self.pkgs[pkg.name] = pkg
+
+    @property
+    def all_sources(self):
+        return sum((pkg.sources for pkg in self.pkgs), [])
 
 
 class MapDataPackage:
@@ -47,3 +54,17 @@ class MapDataPackage:
             raise MapInitError('Map package '+path+' has no name in map.json.')
 
         self.extends = data.get('extends')
+
+        self.size = data.get('size')
+
+        self.sources = tuple(MapSource(self, name, source)
+                             for name, source in data.get('sources', {}).items())
+
+        self.levels = data.get('levels')
+
+
+class MapSource:
+    def __init__(self, pkg, name, data):
+        self.name = name
+        self.src = os.path.join(pkg.path, data['src'])
+        self.bounds = data['bounds']
