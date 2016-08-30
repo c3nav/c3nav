8b05a5eb43fd7f03ceef0250518fe381746b3cf7
Nicole Klünder <nomoketo@nomoketo.de>
2016-08-30 15:54:41 +0200

    introducing the manage.py checkmap command

--- src/c3nav/mapdata/management/commands/dumpmap.py
+++ src/c3nav/mapdata/management/commands/dumpmap.py
@@ -1,17 +1,23 @@
-from django.core.management.base import BaseCommand
-from django.db import transaction
+from django.core.management.base import BaseCommand, CommandError
 
 from ...packageio import write_packages
 
 
 class Command(BaseCommand):
-    help = 'Dump the map database'
+    help = 'Dump the map database into the map package files'
 
     def add_arguments(self, parser):
         parser.add_argument('--no-prettify', dest='prettify', action='store_const', const=False, default=True,
                             help='dont\'t prettify existing files')
+        parser.add_argument('--check-only', action='store_const', const=True, default=False,
+                            help='check if there are files to update')
 
     def handle(self, *args, **options):
-        with transaction.atomic():
-            write_packages(prettify=options['prettify'])
-            print()
+        count = write_packages(prettify=options['prettify'], check_only=options['check_only'])
+        if options['check_only']:
+            if count == 0:
+                print('No errors found!')
+            else:
+                raise CommandError('Found errors in %s file(s)' % count)
+        else:
+            print('%s file(s) affected' % count)
