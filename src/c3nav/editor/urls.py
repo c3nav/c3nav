4e1f43d89bed572c1631c83d4df99c228e2b9c12
Nicole Klünder <nomoketo@nomoketo.de>
2016-09-11 22:12:14 +0200

    sort imports

--- src/c3nav/editor/urls.py
+++ src/c3nav/editor/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import url
 from django.views.generic import TemplateView
 
-
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='editor/map.html'), name='editor.index')
 ]
