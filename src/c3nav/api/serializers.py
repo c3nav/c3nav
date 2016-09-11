04dc68284fee5d9e809d809406682fd19cab18b9
Laura Klünder <laura@codingcatgirl.de>
2016-09-11 22:35:00 +0200

    add public and access to package serialization

--- src/c3nav/api/serializers.py
+++ src/c3nav/api/serializers.py
@@ -1,6 +1,9 @@
+from django.conf import settings
+
 from rest_framework.serializers import ModelSerializer
 
 from ..mapdata.models import Level, Package, Source
+from .permissions import can_access_package
 
 
 class BoundsMixin:
@@ -21,6 +24,13 @@ class PackageSerializer(BoundsMixin, ModelSerializer):
     class Meta:
         model = Package
         fields = ('name', 'depends')
+
+    def to_representation(self, obj):
+        result = super().to_representation(obj)
+        result['public'] = obj.name in settings.PUBLIC_PACKAGES
+        if 'request' in self.context:
+            result['access'] = can_access_package(self.context['request'], obj)
+        return result
 
 
 class SourceSerializer(BoundsMixin, ModelSerializer):
