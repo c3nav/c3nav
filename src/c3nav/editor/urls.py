2dcaab41a8c5195a3ec873ba91c651eca19fd3af
Nicole Klünder <nomoketo@nomoketo.de>
2016-09-11 22:11:28 +0200

    update editor js to only use the REST API.

--- src/c3nav/editor/urls.py
+++ src/c3nav/editor/urls.py
@@ -1,8 +1,7 @@
 from django.conf.urls import url
+from django.views.generic import TemplateView
 
-from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='editor.index'),
-    url(r'^sources/image/(?P<source>[^/]+)$', views.source, name='editor.sources.image'),
+    url(r'^$', TemplateView.as_view(template_name='editor/map.html'), name='editor.index')
 ]
