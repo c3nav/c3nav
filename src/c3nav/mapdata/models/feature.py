52d7b5b121e6e87db6dd87dba6605f969b01c09c
Laura Klünder <laura@codingcatgirl.de>
2016-09-20 11:52:55 +0200

    new map edit control panel

--- src/c3nav/mapdata/models/feature.py
+++ src/c3nav/mapdata/models/feature.py
@@ -1,16 +1,28 @@
+from collections import OrderedDict, namedtuple
+
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
+
+
+class FeatureType(namedtuple('FeatureType', ('name', 'title', 'title_plural', 'geomtype', 'color'))):
+    def __init__(self, *args, **kwartgs):
+        FEATURE_TYPES[self.name] = self
+
+FEATURE_TYPES = OrderedDict()
+FeatureType('building', _('Building'), _('Buildings'), 'polygon', '#333333')
+FeatureType('room', _('Room'), _('Rooms'), 'polygon', '#CCCCCC')
+FeatureType('outside', _('Outside Area'), _('Outside Areas'), 'polygon', '#EEEEEE')
+FeatureType('obstacle', _('Obstacle'), _('Obstacles'), 'polygon', '#999999')
+# FeatureType('door', _('Door'), 'polygon', '#FF00FF')
+# FeatureType('step', _('Step'), 'polyline', '#FF0000')
+# FeatureType('elevator', _('Elevator'), 'polygon', '#99CC00')
 
 
 class Feature(models.Model):
     """
     A map feature
     """
-    TYPES = (
-        ('building', _('Building')),
-        ('room', _('Room')),
-        ('obstacle', _('Obstacle')),
-    )
+    TYPES = tuple((name, t.title) for name, t in FEATURE_TYPES.items())
 
     name = models.SlugField(_('feature identifier'), primary_key=True, max_length=50, help_text=_('e.g. noc'))
     package = models.ForeignKey('Package', on_delete=models.CASCADE, related_name='features',
