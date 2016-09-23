3c96a98344426ff97f375494441fa257f0685b44
Laura Klünder <laura@codingcatgirl.de>
2016-09-23 17:02:17 +0200

    multi-language feature titles

--- src/c3nav/editor/forms.py
+++ src/c3nav/editor/forms.py
@@ -1,5 +1,9 @@
-from django.forms import ModelForm
+from collections import OrderedDict
+
+from django.conf import settings
+from django.forms import CharField, ModelForm, ValidationError
 from django.forms.widgets import HiddenInput
+from django.utils.translation import ugettext_lazy as _
 
 from ..mapdata.models import Feature
 
@@ -11,6 +15,30 @@ class FeatureForm(ModelForm):
         self.fields['level'].widget = HiddenInput()
         self.fields['geometry'].widget = HiddenInput()
 
+        titles = OrderedDict((lang_code, '') for lang_code, language in settings.LANGUAGES)
+        if self.instance is not None and self.instance.pk:
+            titles.update(self.instance.titles)
+
+        language_titles = dict(settings.LANGUAGES)
+        for language in titles.keys():
+            new_title = self.data.get('title_' + language)
+            if new_title is not None:
+                titles[language] = new_title
+            self.fields['title_' + language] = CharField(label=language_titles.get(language, language), required=False,
+                                                         initial=titles[language].strip(), max_length=50)
+        self.titles = titles
+
+    def clean(self):
+        super().clean()
+        if not any(self.titles.values()):
+            raise ValidationError(
+                _('You have to select a title in at least one language.')
+            )
+
+    def get_languages(self):
+        pass
+
     class Meta:
+        # generate extra fields in the number specified via extra_fields
         model = Feature
         fields = ['name', 'package', 'level', 'geometry']
--- src/c3nav/mapdata/models/feature.py
+++ src/c3nav/mapdata/models/feature.py
@@ -2,9 +2,11 @@ from collections import OrderedDict, namedtuple
 
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import get_language
 
 from c3nav.mapdata.models.level import Level
 from c3nav.mapdata.models.package import Package
+
 from ..fields import GeometryField
 
 
@@ -12,11 +14,14 @@ class FeatureType(namedtuple('FeatureType', ('name', 'title', 'title_plural', 'g
     def __init__(self, *args, **kwargs):
         FEATURE_TYPES[self.name] = self
 
+
 FEATURE_TYPES = OrderedDict()
 FeatureType('building', _('Building'), _('Buildings'), 'polygon', '#333333')
 FeatureType('room', _('Room'), _('Rooms'), 'polygon', '#CCCCCC')
 FeatureType('outside', _('Outside Area'), _('Outside Areas'), 'polygon', '#EEEEEE')
 FeatureType('obstacle', _('Obstacle'), _('Obstacles'), 'polygon', '#999999')
+
+
 # FeatureType('door', _('Door'), 'polygon', '#FF00FF')
 # FeatureType('step', _('Step'), 'polyline', '#FF0000')
 # FeatureType('elevator', _('Elevator'), 'polygon', '#99CC00')
@@ -36,9 +41,21 @@ class Feature(models.Model):
                               verbose_name=_('level'))
     geometry = GeometryField()
 
+    @property
+    def titles(self):
+        return {title.language: title.title for title in self.featuretitles.all()}
+
+    @property
+    def title(self):
+        titles = self.titles
+        lang = get_language()
+        if lang in titles:
+            return titles[lang]
+        return next(iter(titles.values())) if titles else self.name
+
 
 class FeatureTitle(models.Model):
-    feature = models.ForeignKey('Feature', on_delete=models.CASCADE, related_name='titles',
+    feature = models.ForeignKey('Feature', on_delete=models.CASCADE, related_name='featuretitles',
                                 verbose_name=_('map package'))
     language = models.CharField(max_length=50)
     title = models.CharField(max_length=50)
