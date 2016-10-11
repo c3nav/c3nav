692c2b7b6acf2a7285fd424217dba575aadabedb
Laura Klünder <laura@codingcatgirl.de>
2016-10-11 17:34:13 +0200

    update map js to remove ?sparse=true

--- src/c3nav/editor/static/editor/js/editor.js
+++ src/c3nav/editor/static/editor/js/editor.js
@@ -53,7 +53,7 @@ editor = {
 
     packages: {},
     get_packages: function () {
-        $.getJSON('/api/packages/?sparse=1', function (packages) {
+        $.getJSON('/api/packages/', function (packages) {
             var bounds = [[0, 0], [0, 0]];
             var pkg;
             for (var i = 0; i < packages.length; i++) {
@@ -70,7 +70,7 @@ editor = {
 
     sources: {},
     get_sources: function () {
-        $.getJSON('/api/sources/?sparse=1', function (sources) {
+        $.getJSON('/api/sources/', function (sources) {
             var layers = {};
             var source;
             for (var i = 0; i < sources.length; i++) {
@@ -87,7 +87,7 @@ editor = {
     _level: null,
     level_feature_layers: {},
     get_levels: function () {
-        $.getJSON('/api/levels/?sparse=1&ordering=-altitude', function (levels) {
+        $.getJSON('/api/levels/?ordering=-altitude', function (levels) {
             L.LevelControl = L.Control.extend({
                 options: {
                     position: 'bottomright'
@@ -186,7 +186,7 @@ editor = {
 
     features: {},
     get_features: function () {
-        $.getJSON('/api/features/?sparse=1', function(features) {
+        $.getJSON('/api/features/', function(features) {
             var feature_type;
             for (var level in editor.levels) {
                 for (var j = 0; j < editor.feature_types_order.length; j++) {
