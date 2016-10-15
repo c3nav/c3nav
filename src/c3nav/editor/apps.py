f00557f420103b007cb7cfc955531e3c754df9ef
Nicole Klünder <nomoketo@nomoketo.de>
2016-10-15 11:23:27 +0200

    fix missing editor forms

--- src/c3nav/editor/apps.py
+++ src/c3nav/editor/apps.py
@@ -6,4 +6,6 @@ class EditorConfig(AppConfig):
 
     def ready(self):
         from c3nav.editor.hosters import init_hosters
+        from c3nav.editor.forms import create_editor_forms
         init_hosters()
+        create_editor_forms()
