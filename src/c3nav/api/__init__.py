78b5f8be71b551be689918539258d4fb62982997
Nicole Klünder <nomoketo@nomoketo.de>
2016-11-14 21:18:42 +0100

    fix flake8

--- src/c3nav/api/__init__.py
+++ src/c3nav/api/__init__.py
@@ -11,5 +11,6 @@ def nicer_renderer(self, data, accepted_media_type=None, renderer_context=None):
         return orig_render(self, data, accepted_media_type, renderer_context)
     return json_encoder_reindent(lambda d: orig_render(self, d, accepted_media_type, renderer_context), data)
 
+
 # Monkey patch for nicer indentation in the django rest framework
 JSONRenderer.render = nicer_renderer
