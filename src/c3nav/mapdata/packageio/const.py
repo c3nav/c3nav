0e64d121d1570a2ab3de885eed8c405a33de53cc
Nicole Klünder <nomoketo@nomoketo.de>
2016-11-26 18:37:26 +0100

    add obstacle support to packageio

--- src/c3nav/mapdata/packageio/const.py
+++ src/c3nav/mapdata/packageio/const.py
@@ -1,4 +1,4 @@
 from c3nav.mapdata.models import Level, Package, Source
-from c3nav.mapdata.models.geometry import Area, Building
+from c3nav.mapdata.models.geometry import Area, Building, Obstacle
 
-ordered_models = (Package, Level, Source, Building, Area)
+ordered_models = (Package, Level, Source, Building, Area, Obstacle)
