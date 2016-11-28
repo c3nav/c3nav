8b19c16e8d194697c8ae511527bee152ad4188c8
Nicole Klünder <nomoketo@nomoketo.de>
2016-11-28 16:27:25 +0100

    add Doors to packageio

--- src/c3nav/mapdata/packageio/const.py
+++ src/c3nav/mapdata/packageio/const.py
@@ -1,4 +1,4 @@
 from c3nav.mapdata.models import Level, Package, Source
-from c3nav.mapdata.models.geometry import Area, Building, Obstacle
+from c3nav.mapdata.models.geometry import Area, Building, Door, Obstacle
 
-ordered_models = (Package, Level, Source, Building, Area, Obstacle)
+ordered_models = (Package, Level, Source, Building, Area, Door, Obstacle)
