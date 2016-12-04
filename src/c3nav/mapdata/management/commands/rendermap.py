01e6674cd01dcf36abd9b5980a8d2dcc343b9a22
Laura Klünder <laura@codingcatgirl.de>
2016-12-04 13:47:36 +0100

    rendermap: add --show-accessibles

--- src/c3nav/mapdata/management/commands/rendermap.py
+++ src/c3nav/mapdata/management/commands/rendermap.py
@@ -6,5 +6,9 @@ from c3nav.mapdata.render import render_all_levels
 class Command(BaseCommand):
     help = 'render the map'
 
+    def add_arguments(self, parser):
+        parser.add_argument('--show-accessibles', action='store_const', const=True, default=False,
+                            help='highlight graph building areas (for debugging, but it looks nice, too)')
+
     def handle(self, *args, **options):
-        render_all_levels()
+        render_all_levels(show_accessibles=options['show_accessibles'])
--- src/c3nav/mapdata/render/__init__.py
+++ src/c3nav/mapdata/render/__init__.py
@@ -2,13 +2,13 @@ from c3nav.mapdata.models import Level
 from c3nav.mapdata.render.renderer import LevelRenderer, get_render_path  # noqa
 
 
-def render_all_levels():
+def render_all_levels(show_accessibles=False):
     renderers = []
     for level in Level.objects.all().order_by('altitude'):
         renderers.append(LevelRenderer(level))
 
     for renderer in renderers:
-        renderer.render_base()
+        renderer.render_base(show_accessibles=show_accessibles)
 
     for renderer in renderers:
         if not renderer.level.intermediate:
