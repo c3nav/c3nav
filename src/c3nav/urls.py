a6ac8900d04125a56b02bc5728e2e5c4275bf79d
Nicole Klünder <nomoketo@nomoketo.de>
2016-12-13 20:17:56 +0100

    add main site base

--- src/c3nav/urls.py
+++ src/c3nav/urls.py
@@ -4,10 +4,12 @@ from django.contrib import admin
 import c3nav.api.urls
 import c3nav.control.urls
 import c3nav.editor.urls
+import c3nav.site.urls
 
 urlpatterns = [
     url(r'^control/', include(c3nav.control.urls)),
     url(r'^editor/', include(c3nav.editor.urls)),
     url(r'^api/', include(c3nav.api.urls, namespace='api')),
     url(r'^admin/', admin.site.urls),
+    url(r'^', include(c3nav.site.urls)),
 ]
