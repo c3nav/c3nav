eb5dc1130adfa6b40c17dcd026c51056e98013c5
Laura Klünder <laura@codingcatgirl.de>
2016-12-18 01:28:08 +0100

    add OneWay. no support in buildgraph yet

--- src/c3nav/mapdata/packageio/const.py
+++ src/c3nav/mapdata/packageio/const.py
@@ -1,8 +1,8 @@
 from c3nav.mapdata.models import AreaLocation, Level, LocationGroup, Package, Source
 from c3nav.mapdata.models.collections import Elevator
 from c3nav.mapdata.models.geometry import (Building, Door, ElevatorLevel, Escalator, EscalatorSlope, Hole,
-                                           LevelConnector, LineObstacle, Obstacle, Outside, Room, Stair)
+                                           LevelConnector, LineObstacle, Obstacle, OneWay, Outside, Room, Stair)
 
 ordered_models = (Package, Level, LevelConnector, Source, Building, Room, Outside, Door, Obstacle, Hole)
-ordered_models += (Elevator, ElevatorLevel, LineObstacle, Stair, Escalator, EscalatorSlope)
+ordered_models += (Elevator, ElevatorLevel, LineObstacle, Stair, Escalator, EscalatorSlope, OneWay)
 ordered_models += (LocationGroup, AreaLocation)
