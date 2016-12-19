02c07fe5cb63b57563fdae893b901539a9c27811
Nicole Klünder <nomoketo@nomoketo.de>
2016-12-19 01:54:27 +0100

    added translation support and german translations

--- src/c3nav/settings.py
+++ src/c3nav/settings.py
@@ -154,6 +154,7 @@ INSTALLED_APPS = [
 MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
