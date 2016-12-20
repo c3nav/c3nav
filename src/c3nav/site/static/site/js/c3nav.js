a6f4b3c9b2fae2065620455f3e2de7d519b814bc
Laura Klünder <laura@codingcatgirl.de>
2016-12-20 20:29:18 +0100

    new ui that also works without javascript

--- src/c3nav/site/static/site/js/c3nav.js
+++ src/c3nav/site/static/site/js/c3nav.js
@@ -1,6 +1,6 @@
 c3nav = {
     init: function() {
-        if (!$('#main_view').length) return;
+        if (!$('.main-view').length) return;
 
         c3nav._typeahead_locations = new Bloodhound({
             datumTokenizer: function(data) {
@@ -29,19 +29,43 @@ c3nav = {
             }
         };
 
-        c3nav.update_history_state(true);
         c3nav.init_typeahead($('.locationselect input:text'));
 
         $('.locationselect:not(.selected) .locationselect-input .tt-input').first().focus();
-        $('.locationselect .icons .reset').click(c3nav._locationselect_reset)
+        $('.locationselect .icons .reset').click(c3nav._locationselect_reset);
+        $('#route-from-here').click(c3nav._click_route_from_here);
+        $('#route-to-here').click(c3nav._click_route_to_here);
+
+        window.onpopstate = c3nav._onpopstate;
     },
 
     _locationselect_reset: function(e) {
         e.preventDefault();
-        var locationselect = $(this).closest('.locationselect');
-        locationselect.find('.id-field').val('');
-        locationselect.removeClass('selected').find('.tt-input').focus().keyup().removeData('enter_item');
-        c3nav.update_history_state();
+        var location_group = $(this).closest('.location-group');
+        location_group.find('.id-field').val('');
+        location_group.removeClass('selected').find('.tt-input').focus().removeData('enter_item');
+        location_group,find('.tt-suggestion').remove();
+        c3nav._locations_changed();
+    },
+    locationselect_focus: function() {
+        $('.location-group:visible:not(.selected) .locationselect-input .tt-input').first().focus();
+    },
+
+    _click_route_from_here: function(e) {
+        c3nav._click_route_x_here(e, $('.origin-select'));
+    },
+    _click_route_to_here: function(e) {
+        c3nav._click_route_x_here(e, $('.destination-select'));
+    },
+    _click_route_x_here: function(e, location_group) {
+        e.preventDefault();
+        $('.main-view').removeClass('mode-location').addClass('mode-route');
+        from_group = $('.location-select');
+        from_group.removeClass('selected');
+        location_group.addClass('selected').find('.id-field').val(from_group.find('.id-field').val());
+        location_group.find('.locationselect-selected .location').html(from_group.find('.locationselect-selected .location').html());
+        c3nav._locations_changed();
+        c3nav.locationselect_focus();
     },
 
     init_typeahead: function(elem) {
@@ -55,6 +79,7 @@ c3nav = {
     },
     _typeahead_keydown: function(e) {
         if (e.which == 13) {
+            e.preventDefault();
             var target = $(e.target);
             enter_item = target.data('enter_item');
             if (enter_item !== undefined) {
@@ -63,16 +88,15 @@ c3nav = {
         }
     },
     _typeahead_select: function(e, item) {
-        var locationselect = $(e.target).closest('.locationselect');
-        locationselect.addClass('selected');
-        var selected = locationselect.find('.locationselect-selected');
+        var location_group = $(e.target).closest('.location-group');
+        location_group.addClass('selected');
+        var selected = location_group.find('.locationselect-selected');
         selected.find('.title').text(item.title);
         selected.find('.subtitle').text(item.subtitle);
         selected.find('.id-field').val(item.id);
         e.target.blur();
-        c3nav.update_history_state();
-
-        $('.locationselect:not(.selected) .locationselect-input .tt-input').first().focus();
+        c3nav._locations_changed();
+        c3nav.locationselect_focus();
     },
     _typeahead_blur: function(e) {
         $(e.target).val('');
@@ -81,24 +105,25 @@ c3nav = {
         $(e.target).data('enter_item', item);
     },
 
-    update_history_state: function(e, replace) {
-        var origin = $(':input[name=origin]').val();
-        var destination = $(':input[name=destination]').val();
-        url = '/';
-        if (origin !== '') {
-            url += origin + '/'
-            if (destination !== '') {
-                url += destination + '/'
-            }
-        } else if (destination !== '') {
-            url += '_/' + destination + '/'
-        }
-        if (replace) {
-            history.replaceState({}, '', url);
+    _locations_changed: function(e) {
+        var url;
+        if ($('.main-view').is('.mode-location')) {
+            url = '/l/'+$(':input[name=location]').val()+'/';
         } else {
-            history.pushState({}, '', url);
+            var origin = $(':input[name=origin]').val();
+            var destination = $(':input[name=destination]').val();
+            if (origin !== '') {
+                url = (destination !== '') ? '/r/'+origin+'/'+destination+'/' : '/o/'+origin+'/';
+            } else {
+                url = (destination !== '') ? '/d/'+destination+'/' : '/';
+            }
+            $('.main-view').toggleClass('can-route', (origin !== '' && destination !== ''));
         }
-
+        history.pushState({}, '', url);
+    },
+    _onpopstate: function() {
+        document.location.href = document.location;
     }
+
 };
 $(document).ready(c3nav.init);
--- src/c3nav/site/urls.py
+++ src/c3nav/site/urls.py
@@ -4,8 +4,9 @@ from c3nav.site.views import level_image, main
 
 urlpatterns = [
     url(r'^map/(?P<level>[a-z0-9-_:]+).png$', level_image, name='site.level_image'),
-    url(r'^(?P<origin>[a-z0-9-_:]+)/$', main, name='site.main'),
-    url(r'^_/(?P<destination>[a-z0-9-_:]+)/$', main, name='site.main'),
-    url(r'^(?P<origin>[a-z0-9-_:]+)/(?P<destination>[a-z0-9-_:]+)/$', main, name='site.main'),
-    url(r'^$', main, name='site.main')
+    url(r'^l/(?P<location>[a-z0-9-_:]+)/$', main, name='site.location'),
+    url(r'^o/(?P<origin>[a-z0-9-_:]+)/$', main, name='site.origin'),
+    url(r'^d/(?P<destination>[a-z0-9-_:]+)/$', main, name='site.destination'),
+    url(r'^r/(?P<origin>[a-z0-9-_:]+)/(?P<destination>[a-z0-9-_:]+)/$', main, name='site.route'),
+    url(r'^$', main, name='site.index')
 ]
