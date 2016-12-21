e95e125e008905dce1bba6b61ca400f991d393c9
Nicole Klünder <nomoketo@nomoketo.de>
2016-12-21 21:28:31 +0100

    cache map_image

--- src/c3nav/site/urls.py
+++ src/c3nav/site/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url
 
-from c3nav.site.views import level_image, main
+from c3nav.site.views import main, map_image
 
 urlpatterns = [
-    url(r'^map/(?P<level>[a-z0-9-_:]+)/(?P<area>[a-z0-9-_:]+).png$', level_image, name='site.level_image'),
+    url(r'^map/(?P<level>[a-z0-9-_:]+)/(?P<area>[a-z0-9-_:]+).png$', map_image, name='site.level_image'),
     url(r'^l/(?P<location>[a-z0-9-_:]+)/$', main, name='site.location'),
     url(r'^o/(?P<origin>[a-z0-9-_:]+)/$', main, name='site.origin'),
     url(r'^d/(?P<destination>[a-z0-9-_:]+)/$', main, name='site.destination'),
