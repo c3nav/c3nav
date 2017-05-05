30e2d392012c167ae768ac296223af4c83c7aa04
Nicole Klünder <nomoketo@nomoketo.de>
2017-05-05 16:37:03 +0200

    flake8 and isort

--- src/c3nav/mapdata/models/__init__.py
+++ src/c3nav/mapdata/models/__init__.py
@@ -1,5 +1,5 @@
 from .level import Level  # noqa
 from .source import Source  # noqa
 from .geometry import LevelFeature  # noqa
-from c3nav.mapdata.models.base import GEOMETRY_FEATURE_TYPES
+from c3nav.mapdata.models.base import GEOMETRY_FEATURE_TYPES  # noqa
 from .locations import AreaLocation, LocationGroup  # noqa
--- src/c3nav/mapdata/models/geometry.py
+++ src/c3nav/mapdata/models/geometry.py
@@ -1,4 +1,5 @@
 from collections import OrderedDict
+
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from shapely.geometry import CAP_STYLE, JOIN_STYLE
