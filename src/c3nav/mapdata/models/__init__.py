f9ed689fb5a38c0e387abb94f2ffe85e1ce9010a
Nicole Klünder <nomoketo@nomoketo.de>
2017-05-05 13:26:25 +0200

    move elevators to area model

--- src/c3nav/mapdata/models/__init__.py
+++ src/c3nav/mapdata/models/__init__.py
@@ -1,6 +1,5 @@
 from .level import Level  # noqa
 from .source import Source  # noqa
-from .collections import Elevator  # noqa
 from .geometry import LevelFeature  # noqa
 from c3nav.mapdata.models.base import GEOMETRY_FEATURE_TYPES
 from .locations import AreaLocation, LocationGroup  # noqa
