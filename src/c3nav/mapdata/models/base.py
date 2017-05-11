513bb25b6a6d3a7b6c4a50446968ddbe62805fb8
Laura Klünder <laura@codingcatgirl.de>
2017-05-11 09:06:19 +0200

    move title property to Location

--- src/c3nav/mapdata/models/base.py
+++ src/c3nav/mapdata/models/base.py
@@ -1,7 +1,6 @@
 from collections import OrderedDict
 
 from django.db import models
-from django.utils.translation import get_language
 
 EDITOR_FORM_MODELS = OrderedDict()
 
@@ -11,12 +10,3 @@ class EditorFormMixin(models.Model):
 
     class Meta:
         abstract = True
-
-    @property
-    def title(self):
-        if not hasattr(self, 'titles'):
-            return self.name
-        lang = get_language()
-        if lang in self.titles:
-            return self.titles[lang]
-        return next(iter(self.titles.values())) if self.titles else self.name
