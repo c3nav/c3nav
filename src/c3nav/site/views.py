c34334c7fe947bb448e3cb147285ab517d6fb6f9
Laura Klünder <laura@codingcatgirl.de>
2017-05-12 13:54:28 +0200

    remove old search implementation

--- src/c3nav/site/views.py
+++ src/c3nav/site/views.py
@@ -11,7 +11,6 @@ from c3nav.access.apply import get_visible_areas
 from c3nav.mapdata.inclusion import get_includables_avoidables, parse_include_avoid
 from c3nav.mapdata.lastupdate import get_last_mapdata_update
 from c3nav.mapdata.models.section import Section
-from c3nav.mapdata.search import search_location
 from c3nav.mapdata.utils.cache import get_sections_cached
 from c3nav.mapdata.utils.misc import get_dimensions, get_render_path
 from c3nav.routing.exceptions import AlreadyThere, NoRouteFound, NotYetRoutable
