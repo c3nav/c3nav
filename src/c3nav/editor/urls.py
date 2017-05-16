a16696a9414c8f5c02fa2ba147721f78504bc186
Laura Klünder <laura@codingcatgirl.de>
2017-05-16 14:10:50 +0200

    editor: restoring editing functionality (for sections for now)

--- src/c3nav/editor/urls.py
+++ src/c3nav/editor/urls.py
@@ -1,14 +1,9 @@
 from django.conf.urls import url
 
-from c3nav.editor.views import edit_mapitem, list_mapitems, list_mapitemtypes, main_index, section_detail
+from c3nav.editor.views import edit, main_index, section_detail
 
 urlpatterns = [
     url(r'^$', main_index, name='editor.index'),
-    url(r'^sections/(?P<section>[0-9]+)/$', section_detail, name='editor.section'),
-    url(r'^sections/(?P<section>[0-9]+)/edit$', section_detail, name='editor.section.edit'),
-    url(r'^mapitemtypes/(?P<level>[^/]+)/$', list_mapitemtypes, name='editor.mapitemtypes'),
-    url(r'^mapitems/(?P<mapitem_type>[^/]+)/list/$', list_mapitems, name='editor.mapitems'),
-    url(r'^mapitems/(?P<mapitem_type>[^/]+)/list/(?P<sectionl>[0-9]+)/$', list_mapitems, name='editor.mapitems.level'),
-    url(r'^mapitems/(?P<mapitem_type>[^/]+)/add/$', edit_mapitem, name='editor.mapitems.add'),
-    url(r'^mapitems/(?P<mapitem_type>[^/]+)/edit/(?P<id>[^/]+)/$', edit_mapitem, name='editor.mapitems.edit'),
+    url(r'^sections/(?P<pk>[0-9]+)/$', section_detail, name='editor.section'),
+    url(r'^sections/(?P<pk>[0-9]+)/edit$', edit, name='editor.section.edit', kwargs={'model': 'Section'}),
 ]
