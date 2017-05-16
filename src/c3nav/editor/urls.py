a053f826b8b58096d4949f16fad378c0d673fbf7
Nicole Klünder <nomoketo@nomoketo.de>
2017-05-16 12:34:45 +0200

    new editor index

--- src/c3nav/editor/urls.py
+++ src/c3nav/editor/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import url
 
-from c3nav.editor.views import edit_mapitem, list_mapitems, list_mapitemtypes, main_index, sections_list
+from c3nav.editor.views import edit_mapitem, list_mapitems, list_mapitemtypes, main_index, section_detail
 
 urlpatterns = [
     url(r'^$', main_index, name='editor.index'),
-    url(r'^sections/$', sections_list, name='editor.sections'),
-    url(r'^sections/(?P<section>[0-9]+)/$', main_index, name='editor.index'),
+    url(r'^sections/(?P<section>[0-9]+)/$', section_detail, name='editor.section'),
+    url(r'^sections/(?P<section>[0-9]+)/edit$', section_detail, name='editor.section.edit'),
     url(r'^mapitemtypes/(?P<level>[^/]+)/$', list_mapitemtypes, name='editor.mapitemtypes'),
     url(r'^mapitems/(?P<mapitem_type>[^/]+)/list/$', list_mapitems, name='editor.mapitems'),
     url(r'^mapitems/(?P<mapitem_type>[^/]+)/list/(?P<sectionl>[0-9]+)/$', list_mapitems, name='editor.mapitems.level'),
