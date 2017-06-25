d86b31d0e90a3500f5ed807da2dea6428cc7787a
Laura Klünder <laura@codingcatgirl.de>
2017-06-25 17:20:54 +0200

    don't show undo button in history, this may be confusing

--- src/c3nav/editor/views/changes.py
+++ src/c3nav/editor/views/changes.py
@@ -123,11 +123,14 @@ def group_changes(changeset, can_edit=False, show_history=False):
             'discarded': change.discarded_by_id is not None,
             'apply_problem': change.check_apply_problem(),
             'has_no_effect': change.check_has_no_effect(),
-            'can_restore': change.can_restore,
         }
         if show_history:
             change_data.update({
                 'created': _('created at %(datetime)s') % {'datetime': date_format(change.created, 'DATETIME_FORMAT')},
+            })
+        else:
+            change_data.update({
+                'can_restore': change.can_restore,
             })
         changes.append(change_data)
 
