0723992ae187a9cc616199ade1192c339b39ea4a
Laura Klünder <laura@codingcatgirl.de>
2017-07-05 18:00:31 +0200

    deactivate the current change set

--- src/c3nav/editor/views/users.py
+++ src/c3nav/editor/views/users.py
@@ -1,5 +1,7 @@
+from django.contrib import messages
 from django.contrib.auth.models import User
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, redirect, render
+from django.utils.translation import ugettext_lazy as _
 
 from c3nav.editor.models import ChangeSet
 from c3nav.editor.views.base import sidebar_view
@@ -11,11 +13,15 @@ def user_detail(request, pk):
     if str(pk) != str(user.pk):
         user = get_object_or_404(User, pk=pk)
 
-    qs = ChangeSet.objects.filter(author=user).order_by('-last_update')[:10]
+    if request.method == 'POST':
+        if request.POST.get('deactivate_changeset') == '1' and request.user == user and 'changeset' in request.session:
+            request.session.pop('changeset', None)
+            messages.success(request, _('You deactivated your current changeset.'))
+            return redirect(request.path)
 
     ctx = {
         'user': user,
-        'changesets': qs,
+        'recent_changesets': ChangeSet.objects.filter(author=user).order_by('-last_update')[:10],
     }
 
     return render(request, 'editor/user.html', ctx)
