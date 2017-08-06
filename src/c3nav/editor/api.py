9f476ff31c1c96ed3ca996e8c8ce32e74fd0f8a5
Laura Klünder <laura@codingcatgirl.de>
2017-08-06 18:09:01 +0200

    dont show lower lower spaces on spaces in levels on top

--- src/c3nav/editor/api.py
+++ src/c3nav/editor/api.py
@@ -126,6 +126,8 @@ class EditorViewSet(ViewSet):
             doors_space_geom = cascaded_union([door.geometry for door in doors]+[space.geometry])
 
             levels, levels_on_top, levels_under = self._get_levels_pk(request, level.primary_level)
+            if level.on_top_of_id is not None:
+                levels = chain([level], levels_on_top)
             other_spaces = Space.objects.filter(space_q_for_request, level__pk__in=levels).prefetch_related('groups')
 
             space = next(s for s in other_spaces if s.pk == space.pk)
