02c162e303f407a204196c3f0a01b0bda42a1674
Laura Klünder <laura@codingcatgirl.de>
2017-10-19 22:41:22 +0200

    minor regex improvement

--- src/c3nav/mapdata/utils/svg.py
+++ src/c3nav/mapdata/utils/svg.py
@@ -139,7 +139,7 @@ class SVGImage:
 
     def _trim_decimals(self, data):
         # remove trailing zeros from a decimal
-        return re.sub(r'([0-9]+)\.0', r'\1', re.sub(r'([0-9]+\.[0-9])[0-9]+', r'\1', data))
+        return re.sub(r'([0-9]+)((\.[1-9])[0-9]+|\.[0-9]+)?', r'\1\3', data)
 
     def _create_geometry(self, geometry):
         # convert a shapely geometry into an svg xml element
