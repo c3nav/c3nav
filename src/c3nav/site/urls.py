0f137bab81a2c98a7d88ebcd1cfebc53b84e9955
Laura Klünder <laura@codingcatgirl.de>
2017-10-31 16:09:10 +0100

    x y and zoom can start with a minus

--- src/c3nav/site/urls.py
+++ src/c3nav/site/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 
 from c3nav.site.views import map_index
 
-pos = r'(@(?P<level>[a-z0-9-_:]+),(?P<x>\d+(\.\d+)?),(?P<y>\d+(\.\d+)?),(?P<zoom>\d+(\.\d+)?))?'
+pos = r'(@(?P<level>[a-z0-9-_:]+),(?P<x>-?\d+(\.\d+)?),(?P<y>-?\d+(\.\d+)?),(?P<zoom>-?\d+(\.\d+)?))?'
 
 urlpatterns = [
     url(r'^r/(?P<origin>([a-z0-9-_:]+)?)/(?P<destination>([a-z0-9-_:]+)?)/%s$' % pos, map_index, name='site.index'),
