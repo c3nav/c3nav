36146ac9542fdccb0d503d8d81a2a881dbabbbe4
Laura Klünder <laura@codingcatgirl.de>
2017-11-05 11:24:53 +0100

    c3nav.mapdata.render.image → c3nav.mapdata.render

--- src/c3nav/mapdata/render/image/__init__.py
+++
@@ -1,3 +0,0 @@
-from c3nav.mapdata.render.image.renderer import ImageRenderer  # noqa
-from c3nav.mapdata.render.image.utils import (get_render_level_ids, set_tile_access_cookie,  # noqa
-                                              get_tile_access_cookie)  # noqa
