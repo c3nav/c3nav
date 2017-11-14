1271789d0696ba46031dad5a0c22235d3c329465
Laura Klünder <laura@codingcatgirl.de>
2017-11-14 04:21:20 +0100

    fix walls once and for all hopefully

--- src/c3nav/mapdata/render/data.py
+++ src/c3nav/mapdata/render/data.py
@@ -598,7 +598,7 @@ class LevelGeometries:
         self.walls_bottom = HybridGeometry(self.walls.geom, self.walls.faces)
 
         self.walls.build_polyhedron(self._create_polyhedron,
-                                    lower=vertex_altitudes,
+                                    lower=vertex_altitudes - int(0.7 * 1000),
                                     upper=vertex_wall_heights)
 
         if interpolator is not None:
