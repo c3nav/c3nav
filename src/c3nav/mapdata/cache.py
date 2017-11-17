6da6c5dcdf1501a5c38fc23afad7158bed4237d5
Laura Klünder <laura@codingcatgirl.de>
2017-11-17 15:03:56 +0100

    min zero in GeometryIndexed.to_image

--- src/c3nav/mapdata/cache.py
+++ src/c3nav/mapdata/cache.py
@@ -171,7 +171,7 @@ class GeometryIndexed:
                                int(math.ceil((maxx-minx)/self.resolution))), dtype=np.uint8)
 
         if self.data.size:
-            minval = max(self.data.min(), 0)
+            minval = min(self.data.min(), 0)
             maxval = max(self.data.max(), minval+0.01)
             visible_data = ((self.data.astype(float)-minval)*255/(maxval-minval)).clip(0, 255).astype(np.uint8)
             image_data[self.y:self.y+height, self.x:self.x+width] = visible_data
