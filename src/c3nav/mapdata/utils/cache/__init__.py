83de101969c9e1a0da9d95732ef0fd71ab2d4a52
Laura Klünder <laura@codingcatgirl.de>
2017-11-20 21:14:46 +0100

    import CachePackage in __init__

--- src/c3nav/mapdata/utils/cache/__init__.py
+++ src/c3nav/mapdata/utils/cache/__init__.py
@@ -2,3 +2,4 @@ from c3nav.mapdata.utils.cache.indexed import GeometryIndexed  # noqa
 from c3nav.mapdata.utils.cache.maphistory import MapHistory  # noqa
 from c3nav.mapdata.utils.cache.accessrestrictions import AccessRestrictionAffected  # noqa
 from c3nav.mapdata.utils.cache.changes import changed_geometries  # noqa
+from c3nav.mapdata.utils.cache.package import CachePackage  # noqa
