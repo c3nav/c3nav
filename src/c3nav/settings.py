0485e02a3a22c5e9cf2426c8259502faca81bacd
Laura Klünder <laura@codingcatgirl.de>
2017-11-28 11:24:18 +0100

    remove INSTANCE_NAME

--- src/c3nav/settings.py
+++ src/c3nav/settings.py
@@ -13,8 +13,6 @@ config = configparser.RawConfigParser()
 config.read(['/etc/c3nav/c3nav.cfg', os.path.expanduser('~/.c3nav.cfg'), os.environ.get('C3NAV_CONFIG', 'c3nav.cfg')],
             encoding='utf-8')
 
-INSTANCE_NAME = config.get('c3nav', 'name', fallback='noname')
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 DATA_DIR = config.get('c3nav', 'datadir', fallback=os.environ.get('DATA_DIR', 'data'))
@@ -169,10 +167,10 @@ TILE_ACCESS_COOKIE_DOMAIN = config.get('c3nav', 'tile_access_cookie_domain', fal
 # Internal settings
 STATIC_ROOT = os.path.join(os.path.dirname(__file__), 'static.dist')
 
-SESSION_COOKIE_NAME = 'c3nav_%s_session' % INSTANCE_NAME
-LANGUAGE_COOKIE_NAME = 'c3nav_%s_language' % INSTANCE_NAME
-CSRF_COOKIE_NAME = 'c3nav_%s_csrftoken' % INSTANCE_NAME
-TILE_ACCESS_COOKIE_NAME = 'c3nav_%s_tile_access' % INSTANCE_NAME
+SESSION_COOKIE_NAME = 'c3nav_session'
+LANGUAGE_COOKIE_NAME = 'c3nav_language'
+CSRF_COOKIE_NAME = 'c3nav_csrftoken'
+TILE_ACCESS_COOKIE_NAME = 'c3nav_tile_access'
 SESSION_COOKIE_HTTPONLY = True
 
 # Application definition
