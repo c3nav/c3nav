4195a1325c850e04afd38010c2493812f12c97fa
Laura Klünder <laura@codingcatgirl.de>
2017-11-28 21:25:43 +0100

    show/hide "show in editor" link

--- src/c3nav/site/static/site/js/c3nav.js
+++ src/c3nav/site/static/site/js/c3nav.js
@@ -197,7 +197,12 @@ c3nav = {
             }
         }
         $location_details.find('.details-body').html('').append(elem);
-        $location_details.removeClass('loading').find('.editor').toggle(data.editor_url).attr('href', data.editor_url);
+        var $editor = $location_details.removeClass('loading').find('.editor');
+        if (data.editor_url) {
+            $editor.attr('href', data.editor_url).show();
+        } else {
+            $editor.hide();
+        }
     },
     load_route: function (origin, destination, nofly) {
         var $route = $('#route-summary');
