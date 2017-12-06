8f5010ee2ac338a972d779a5281be8a7494ccb85
Laura Klünder <laura@codingcatgirl.de>
2017-12-06 23:57:01 +0100

    remove debug output

--- src/c3nav/site/static/site/js/c3nav.js
+++ src/c3nav/site/static/site/js/c3nav.js
@@ -466,7 +466,6 @@ c3nav = {
         url = c3nav._build_state_url(state);
         $share.find('img').attr('src', '/qr'+url);
         $share.find('input').val(window.location.protocol+'//'+window.location.host+url)[0].select();
-        console.log(url);
     },
 
     // location inputs
