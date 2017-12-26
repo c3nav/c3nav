ed43b75c6e579777f7fee290e22d2c78370ab1ef
Laura Klünder <laura@codingcatgirl.de>
2017-12-26 23:03:33 +0100

    load searchable locations less often if there is a problem

--- src/c3nav/site/static/site/js/c3nav.js
+++ src/c3nav/site/static/site/js/c3nav.js
@@ -50,7 +50,7 @@ c3nav = {
     },
     load_searchable_locations: function() {
         $.getJSON('/api/locations/?searchable', c3nav._searchable_locations_loaded).fail(function() {
-            window.setTimeout(c3nav.load_searchable_locations, 5000);
+            window.setTimeout(c3nav.load_searchable_locations, 120000);
         });
     },
     _searchable_locations_loaded: function(data) {
