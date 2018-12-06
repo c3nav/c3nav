91b302874eab47978e901be0c39df1fb1b5c7b53
Laura Klünder <laura@codingcatgirl.de>
2018-12-06 03:59:35 +0100

    draw missing geometries (but openscad breaks now)

--- src/c3nav/mapdata/render/engines/openscad.py
+++ src/c3nav/mapdata/render/engines/openscad.py
@@ -161,13 +161,16 @@ class OpenSCADEngine(Base3DEngine):
                             )
 
                         # actual thingy
-                        polygon = self._add_polygon(None, inside_geometry,
-                                                    current_upper_bound - 1, max_slope_altitude+10)
-                        slope = self._add_slope(bounds, altitudearea.altitude, altitudearea.altitude2,
-                                                altitudearea.point1, altitudearea.point2)
+                        polygon = self._add_polygon(None, outside_geometry,
+                                                    min_slope_altitude - 710, max_slope_altitude + 10)
+                        slope1 = self._add_slope(bounds, altitudearea.altitude, altitudearea.altitude2,
+                                                 altitudearea.point1, altitudearea.point2, bottom=False)
+                        slope2 = self._add_slope(bounds, altitudearea.altitude - 700, altitudearea.altitude2 - 700,
+                                                 altitudearea.point1, altitudearea.point2, bottom=True)
                         if slopes:
                             main_building_block.append(
-                                OpenScadBlock('difference()', children=[polygon, slope], comment=name+' inside')
+                                OpenScadBlock('difference()',
+                                              children=[polygon, slope1, slope2], comment=name + 'outside')
                             )
                     else:
                         if altitudearea.altitude < current_upper_bound:
