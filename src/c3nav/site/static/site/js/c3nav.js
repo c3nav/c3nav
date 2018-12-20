f1b69cba3a410fb9fb1c248bfd13e4a0c9c2ced8
Laura Klünder <laura@codingcatgirl.de>
2018-12-20 18:16:29 +0100

    improve search matching even more, wow

--- src/c3nav/site/static/site/js/c3nav.js
+++ src/c3nav/site/static/site/js/c3nav.js
@@ -878,7 +878,8 @@ c3nav = {
         if (a[1] !== b[1]) return b[1] - a[1];
         if (a[2] !== b[2]) return b[2] - a[2];
         if (a[3] !== b[3]) return b[3] - a[3];
-        return a[4] - b[4];
+        if (a[4] !== b[4]) return b[4] - a[4];
+        return a[5] - b[5];
     },
     _locationinput_input: function () {
         var matches = [],
@@ -940,7 +941,7 @@ c3nav = {
                 }
             }
 
-            matches.push([location.elem, leading_words_count, words_total_count, words_start_count, i])
+            matches.push([location.elem, leading_words_count, words_total_count, words_start_count, -location.title.length, i])
         }
 
         matches.sort(c3nav._locationinput_matches_compare);
