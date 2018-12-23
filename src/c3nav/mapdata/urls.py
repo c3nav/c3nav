3a42af568a48b9b4e912b00ad7a21b95a7d04671
Laura Klünder <laura@codingcatgirl.de>
2018-12-23 20:49:47 +0100

    fix tile urls

--- src/c3nav/mapdata/urls.py
+++ src/c3nav/mapdata/urls.py
@@ -4,7 +4,7 @@ from c3nav.mapdata.views import get_cache_package, map_history, tile
 
 urlpatterns = [
     url(r'^(?P<level>\d+)/(?P<zoom>-?\d+)/(?P<x>-?\d+)/(?P<y>-?\d+).png$', tile, name='mapdata.tile'),
-    url(r'^(?P<level>\d+)/(?P<zoom>-?\d+)/(?P<x>-?\d+)/(?P<y>-?\d+)/(?P<access_permissions>\d(-\d)*).png$', tile,
+    url(r'^(?P<level>\d+)/(?P<zoom>-?\d+)/(?P<x>-?\d+)/(?P<y>-?\d+)/(?P<access_permissions>\d+(-\d+)*).png$', tile,
         name='mapdata.tile'),
     url(r'^history/(?P<level>\d+)/(?P<mode>base|composite)\.(?P<filetype>png|data)$', map_history,
         name='mapdata.map_history'),
