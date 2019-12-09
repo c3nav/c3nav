7e48101fa574dc14cd18f65da47529605b0c17f4
Laura Klünder <laura@codingcatgirl.de>
2019-12-09 22:16:38 +0100

    fix imports

--- src/c3nav/mapdata/models/__init__.py
+++ src/c3nav/mapdata/models/__init__.py
@@ -1,8 +1,8 @@
-from c3nav.mapdata.models.access import AccessRestriction  # noqa
-from c3nav.mapdata.models.geometry.level import AltitudeArea, Building, Door, Space  # noqa
-from c3nav.mapdata.models.geometry.space import AltitudeMarker, Area, Hole, LineObstacle, Obstacle, Stair  # noqa
-from c3nav.mapdata.models.graph import GraphEdge, GraphNode, WayType  # noqa
-from c3nav.mapdata.models.level import Level  # noqa
-from c3nav.mapdata.models.locations import Location, LocationGroup, LocationGroupCategory, LocationSlug  # noqa
-from c3nav.mapdata.models.source import Source  # noqa
 from c3nav.mapdata.models.update import MapUpdate  # noqa
+from c3nav.mapdata.models.access import AccessRestriction  # noqa
+from c3nav.mapdata.models.level import Level  # noqa
+from c3nav.mapdata.models.geometry.level import Building, Space, Door, AltitudeArea  # noqa
+from c3nav.mapdata.models.geometry.space import Area, Stair, Obstacle, LineObstacle, Hole, AltitudeMarker  # noqa
+from c3nav.mapdata.models.locations import Location, LocationSlug, LocationGroup, LocationGroupCategory  # noqa
+from c3nav.mapdata.models.source import Source  # noqa
+from c3nav.mapdata.models.graph import GraphNode, WayType, GraphEdge  # noqa
--- src/c3nav/mapdata/render/geometry/__init__.py
+++ src/c3nav/mapdata/render/geometry/__init__.py
@@ -1,3 +1,3 @@
-from c3nav.mapdata.render.geometry.altitudearea import AltitudeAreaGeometries  # noqa
-from c3nav.mapdata.render.geometry.hybrid import HybridGeometry, hybrid_union  # noqa
+from c3nav.mapdata.render.geometry.hybrid import hybrid_union, HybridGeometry  # noqa
 from c3nav.mapdata.render.geometry.level import LevelGeometries  # noqa
+from c3nav.mapdata.render.geometry.altitudearea import AltitudeAreaGeometries  # noqa
