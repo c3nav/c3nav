c8d0a8f5ee4e206755e0c269a4fcf616116c59a1
Laura Klünder <laura@codingcatgirl.de>
2019-12-28 19:47:18 +0100

    you should be able to set coordinates_id to null in forms

--- src/c3nav/mapdata/models/locations.py
+++ src/c3nav/mapdata/models/locations.py
@@ -567,7 +567,7 @@ class Position(CustomLocationProxyMixin, models.Model):
     last_coordinates_update = models.DateTimeField(_('last coordinates update'), null=True)
     timeout = models.PositiveSmallIntegerField(_('timeout (in seconds)'), default=0, blank=True,
                                                help_text=_('0 for no timeout'))
-    coordinates_id = models.CharField(_('coordinates'), null=True, max_length=48)
+    coordinates_id = models.CharField(_('coordinates'), null=True, blank=True, max_length=48)
     api_secret = models.CharField(_('api secret'), max_length=64, default=get_position_api_secret)
 
     can_search = True
