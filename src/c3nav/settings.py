e9a255b095220a5bd1fb7492e0fe3149a93d85f4
Jenny Danzmayr <mail@evilscientress.de>
2023-12-05 00:47:49 +0100

    fixed bug introduced in last commit

--- src/c3nav/settings.py
+++ src/c3nav/settings.py
@@ -198,7 +198,7 @@ MEDIA_URL = config.get('django', 'media_url', fallback='/media/', env='C3NAV_MED
 
 ALLOWED_HOSTS = [n for n in config.get('django', 'allowed_hosts', fallback='*').split(',') if n]
 
-if config.getboolean('django', 'reverse_proxy'):
+if config.getboolean('django', 'reverse_proxy', fallback=False):
     SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
 
