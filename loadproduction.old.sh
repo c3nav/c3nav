e1647fe331996081968d4ffb7504141bdaf25c07
dfjera <dfjera@dfjera.eu>
2024-02-09 22:26:50 +0100

    cleanup

--- loadproduction.old.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-export C3NAV_CONFIG=/home/laura/Projekte/c3nav/data/c3nav.cfg
-ssh root@34c3.c3nav.de "c3nav-manage 34c3 dumpdata mapdata --format json | gzip" | gunzip > production.json
-python src/manage.py loaddata --app mapdata production.json
-# rm production.json
