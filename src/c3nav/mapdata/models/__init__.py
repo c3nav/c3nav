4ab91d54ea903026229454f759daedd9cac4bf86
Laura Klünder <laura@codingcatgirl.de>
2024-08-18 21:49:16 +0200

    missing Theme import in models.__init__

--- src/c3nav/mapdata/models/__init__.py
+++ src/c3nav/mapdata/models/__init__.py
@@ -7,3 +7,4 @@ from c3nav.mapdata.models.geometry.space import Area, Stair, Obstacle, LineObsta
 from c3nav.mapdata.models.locations import Location, LocationSlug, LocationGroup, LocationGroupCategory  # noqa
 from c3nav.mapdata.models.source import Source  # noqa
 from c3nav.mapdata.models.graph import GraphNode, WayType, GraphEdge  # noqa
+from c3nav.mapdata.models.theme import Theme  # noqa
