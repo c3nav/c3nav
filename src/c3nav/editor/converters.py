027bdb83b17ef50efae762df7d61b598caeb5cef
Laura Klünder <laura@codingcatgirl.de>
2024-08-26 17:10:05 +0200

    remove editpk url converter

--- src/c3nav/editor/converters.py
+++
@@ -1,8 +0,0 @@
-class EditPkConverter:
-    regex = r'c?\d+'
-
-    def to_python(self, value):
-        return value
-
-    def to_url(self, value):
-        return value
--- src/c3nav/editor/urls.py
+++ src/c3nav/editor/urls.py
@@ -1,13 +1,10 @@
 from django.apps import apps
 from django.urls import path, register_converter
 
-from c3nav.editor.converters import EditPkConverter
 from c3nav.editor.views.account import change_password_view, login_view, logout_view, register_view
 from c3nav.editor.views.changes import changeset_detail, changeset_edit, changeset_redirect
 from c3nav.editor.views.edit import edit, graph_edit, level_detail, list_objects, main_index, sourceimage, space_detail
 from c3nav.editor.views.users import user_detail, user_redirect
-
-register_converter(EditPkConverter, 'editpk')
 
 
 def add_editor_urls(model_name, parent_model_name=None, with_list=True, explicit_edit=False):
@@ -16,7 +13,7 @@ def add_editor_urls(model_name, parent_model_name=None, with_list=True, explicit
     if parent_model_name:
         parent_model = apps.get_model('mapdata', parent_model_name)
         parent_model_name_plural = parent_model._meta.default_related_name
-        prefix = (parent_model_name_plural+r'/<editpk:'+parent_model_name.lower()+'>/')+model_name_plural
+        prefix = (parent_model_name_plural+r'/<int:'+parent_model_name.lower()+'>/')+model_name_plural
     else:
         prefix = model_name_plural
 
@@ -28,7 +25,7 @@ def add_editor_urls(model_name, parent_model_name=None, with_list=True, explicit
     if with_list:
         result.append(path(prefix+'/', list_objects, name=name_prefix+'list', kwargs=kwargs))
     result.extend([
-        path(prefix+'/<editpk:pk>/'+explicit_edit, edit, name=name_prefix+'edit', kwargs=kwargs),
+        path(prefix+'/<int:pk>/'+explicit_edit, edit, name=name_prefix+'edit', kwargs=kwargs),
         path(prefix+'/create', edit, name=name_prefix+'create', kwargs=kwargs),
     ])
     return result
@@ -36,15 +33,15 @@ def add_editor_urls(model_name, parent_model_name=None, with_list=True, explicit
 
 # todo: custom path converters
 urlpatterns = [
-    path('levels/<editpk:pk>/', level_detail, name='editor.levels.detail'),
-    path('levels/<editpk:level>/spaces/<editpk:pk>/', space_detail, name='editor.spaces.detail'),
-    path('levels/<editpk:on_top_of>/levels_on_top/create', edit, {'model': 'Level'},
+    path('levels/<int:pk>/', level_detail, name='editor.levels.detail'),
+    path('levels/<int:level>/spaces/<int:pk>/', space_detail, name='editor.spaces.detail'),
+    path('levels/<int:on_top_of>/levels_on_top/create', edit, {'model': 'Level'},
          name='editor.levels_on_top.create'),
-    path('levels/<editpk:level>/graph/', graph_edit, name='editor.levels.graph'),
-    path('spaces/<editpk:space>/graph/', graph_edit, name='editor.spaces.graph'),
+    path('levels/<int:level>/graph/', graph_edit, name='editor.levels.graph'),
+    path('spaces/<int:space>/graph/', graph_edit, name='editor.spaces.graph'),
     path('changeset/', changeset_redirect, name='editor.changesets.current'),
-    path('changesets/<editpk:pk>/', changeset_detail, name='editor.changesets.detail'),
-    path('changesets/<editpk:pk>/edit', changeset_edit, name='editor.changesets.edit'),
+    path('changesets/<int:pk>/', changeset_detail, name='editor.changesets.detail'),
+    path('changesets/<int:pk>/edit', changeset_edit, name='editor.changesets.edit'),
     path('sourceimage/<str:filename>', sourceimage, name='editor.sourceimage'),
     path('user/', user_redirect, name='editor.users.redirect'),
     path('users/<int:pk>/', user_detail, name='editor.users.detail'),
