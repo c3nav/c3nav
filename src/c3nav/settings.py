e1ee9bdcf5855a27194c9fc2937ab7c873e5e31e
Jenny Danzmayr <mail@evilscientress.de>
2024-09-17 17:54:38 +0200

    fixed extra static dirs option

--- src/c3nav/settings.py
+++ src/c3nav/settings.py
@@ -515,7 +515,7 @@ BOOTSTRAP3 = {
 
 STATICFILES_DIRS = [
     BASE_DIR / 'c3nav' / 'static',
-    *config.get('c3nav', 'extra_static_dirs', fallback=''),
+    *config.getlist('c3nav', 'extra_static_dirs', fallback=''),
 ]
 
 COMPRESS_PRECOMPILERS = (
