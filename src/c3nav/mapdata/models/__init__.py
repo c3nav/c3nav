33acf2672356ff666bc40d358ed32632739f8fcf
Laura Klünder <laura@codingcatgirl.de>
2025-01-28 15:51:59 +0100

    restructure locations: avoid excessive queries for SPecificLocation

--- src/c3nav/mapdata/models/__init__.py
+++ src/c3nav/mapdata/models/__init__.py
@@ -3,7 +3,7 @@ from c3nav.mapdata.models.access import AccessRestriction  # noqa
 from c3nav.mapdata.models.altitudes import GroundAltitude  # noqa
 from c3nav.mapdata.models.level import Level  # noqa
 from c3nav.mapdata.models.geometry.level import Building, Space, Door, AltitudeArea  # noqa
-from c3nav.mapdata.models.geometry.space import Area, Stair, Obstacle, LineObstacle, Hole, AltitudeMarker  # noqa
+from c3nav.mapdata.models.geometry.space import Area, POI, Stair, Obstacle, LineObstacle, Hole, AltitudeMarker  # noqa
 from c3nav.mapdata.models.locations import Location, LocationSlug, LocationGroup, LocationGroupCategory  # noqa
 from c3nav.mapdata.models.source import Source  # noqa
 from c3nav.mapdata.models.graph import GraphNode, WayType, GraphEdge  # noqa
