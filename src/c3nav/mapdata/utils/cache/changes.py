594439465ecd1cfb91aa63100310ebf6bde9d09d
Laura Klünder <laura@codingcatgirl.de>
2025-01-28 15:51:59 +0100

    restructure locations: avoid excessive queries for SPecificLocation

--- src/c3nav/mapdata/utils/cache/changes.py
+++ src/c3nav/mapdata/utils/cache/changes.py
@@ -1,11 +1,10 @@
 import os
 
-from django.db.models.signals import m2m_changed, post_delete
+from django.db.models.signals import m2m_changed
 from shapely.ops import unary_union
 
 from c3nav.mapdata.models.update import MapUpdateTuple
 from c3nav.mapdata.utils.cache.maphistory import MapHistory
-from c3nav.mapdata.utils.models import get_submodels
 
 
 class GeometryChangeTracker:
