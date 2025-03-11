8322e67fe371768f9235052ddef6dbd14ddd9f5b
Laura Klünder <laura@codingcatgirl.de>
2025-03-11 12:11:22 +0100

    convert get_geometry() into geometries_by_level property

--- src/c3nav/api/schema.py
+++ src/c3nav/api/schema.py
@@ -208,7 +208,7 @@ class AnyGeometrySchema(BaseSchema):
     coordinates: Any
 
 
-GeometryByLevelSchema: TypeAlias = dict[int, list[GeometrySchema]]
+GeometriesByLevelSchema: TypeAlias = dict[int, list[GeometrySchema]]
 
 
 class StatsSchema(BaseSchema):
