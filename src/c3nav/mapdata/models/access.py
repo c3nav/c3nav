a70173568d72a44f7e84fc0caec7e04edbd8e0e3
Laura Klünder <laura@codingcatgirl.de>
2025-03-13 20:18:44 +0100

    fix effective_access_restrictions logic

--- src/c3nav/mapdata/models/access.py
+++ src/c3nav/mapdata/models/access.py
@@ -448,6 +448,6 @@ class AccessRestrictionMixin(AccessRestrictionLogicMixin, models.Model):
     @cached_property
     def effective_access_restrictions(self) -> set[int]:
         return (
-            super().effective_access_restrictions &
+            super().effective_access_restrictions |
             ({self.access_restriction_id} if self.access_restriction_id else set())
         )
