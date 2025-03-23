6d55898e9d3d32792aaf4a8680c2f84574bf775e
Laura Klünder <laura@codingcatgirl.de>
2025-03-24 00:33:46 +0100

    split up processupdates into multiple steps/tasks/etc (fixes #231)

--- src/c3nav/mapdata/management/commands/processupdates.py
+++ src/c3nav/mapdata/management/commands/processupdates.py
@@ -1,23 +1,15 @@
-import logging
-
 from django.conf import settings
 from django.core.management.base import BaseCommand
-from django.db import DatabaseError
 from django.utils.translation import gettext_lazy as _
 
-from c3nav.mapdata.tasks import process_map_updates
+from c3nav.mapdata.updatejobs import run_mapupdate_jobs
 
 
 class Command(BaseCommand):
     help = 'process unprocessed map updates'
 
     def handle(self, *args, **options):
-        logger = logging.getLogger('c3nav')
-
-        try:
-            process_map_updates()
-        except DatabaseError:
-            logger.error(_('There is already map update processing in progress.'))
+        run_mapupdate_jobs()
 
         if not settings.HAS_REAL_CACHE:
             print(_('You have no external cache configured, so don\'t forget to restart your c3nav instance!'))
