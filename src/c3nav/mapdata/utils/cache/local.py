04406a5b5e02c24c5129f4a117e0c8f2d3ddc94f
Laura Klünder <laura@codingcatgirl.de>
2025-03-24 15:43:59 +0100

    clean up access permission cache key generation and api validation

--- src/c3nav/mapdata/utils/cache/local.py
+++ src/c3nav/mapdata/utils/cache/local.py
@@ -89,4 +89,5 @@ class RequestLocalCacheProxy(LocalCacheProxy):
         pass
 
 
+# todo: we want multiple copies of this
 per_request_cache = RequestLocalCacheProxy(maxsize=settings.CACHE_SIZE_LOCATIONS)
