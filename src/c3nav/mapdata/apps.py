c886e4408e54a662f39d74a3a570c10ed757da8f
Laura Klünder <laura@codingcatgirl.de>
2025-04-10 18:25:39 +0200

    migrate part of locationgroup signals to hierarchy signals and move them

--- src/c3nav/mapdata/apps.py
+++ src/c3nav/mapdata/apps.py
@@ -5,7 +5,5 @@ class MapdataConfig(AppConfig):
     name = 'c3nav.mapdata'
 
     def ready(self):
-        from c3nav.mapdata.utils.cache.changes import register_signals
-        register_signals()
         import c3nav.mapdata.metrics  # noqa
         import c3nav.mapdata.updatejobs  # noqa
