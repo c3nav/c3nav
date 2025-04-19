960a47d07c9083427bf522a4e7fd9b27ad4530dd
Laura Klünder <laura@codingcatgirl.de>
2025-04-19 18:26:12 +0200

    add todo for compress_sorted_list_of_int

--- src/c3nav/mapdata/utils/cache/compress.py
+++ src/c3nav/mapdata/utils/cache/compress.py
@@ -14,6 +14,7 @@ def get_type_for_size(num_bits) -> int:
 
 
 def compress_sorted_list_of_int(values: Sequence[int]) -> bytes:
+    # todo: compress? speedup, optimize?
     if not values:
         return b""
     value = next(iter_values := iter(values))
