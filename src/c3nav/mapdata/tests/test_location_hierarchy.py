2e1d097a699d1f7ed473201d458422e049c167dc
Laura Klünder <laura@codingcatgirl.de>
2025-04-25 18:31:07 +0200

    write more location hierarchy tests and fix hierarchy bugs

--- src/c3nav/mapdata/tests/test_location_hierarchy.py
+++ src/c3nav/mapdata/tests/test_location_hierarchy.py
@@ -1,30 +1,41 @@
 from collections import defaultdict
+from typing import Callable
 
 from django.db.models.expressions import F
 from django.db.models.query_utils import Q
+from django.db.utils import IntegrityError
 from django.test.testcases import TransactionTestCase
 
-from c3nav.mapdata.models.locations import DefinedLocation, LocationAdjacency, LocationRelationPath, LocationRelation
-
+from c3nav.mapdata.models.locations import DefinedLocation, LocationAdjacency, LocationRelationPath, LocationRelation, \
+    CircularyHierarchyError
 
 type LocationHierarchyState = dict[tuple[int, int], set[tuple[int, ...]]]
+type UpdateRelationFunc = Callable[[DefinedLocation, DefinedLocation], None]
 
 
 class LocationHierarchyTests(TransactionTestCase):
     def assertHierarchyState(self, state: LocationHierarchyState, msg="location hierarchy doesn't match"):
-        self.assertQuerySetEqual(LocationRelationPath.objects.exclude(
+        self.assertQuerySetEqual(LocationRelationPath.objects.select_related(
+            "prev_path", "adjacency", "relation"
+        ).exclude(
             adjacency__child=F("relation__descendant")
         ), [], msg="Found relation path segment where adjacency child doesn't match relation descendant")
 
-        self.assertQuerySetEqual(LocationRelationPath.objects.exclude(
+        self.assertQuerySetEqual(LocationRelationPath.objects.select_related(
+            "prev_path", "adjacency", "relation"
+        ).exclude(
             Q(prev_path__isnull=True, num_hops=0) | Q(prev_path__num_hops=F("num_hops")-1)
         ), [], msg="Found relation path segment with num_hops not matching prev_path")
 
-        self.assertQuerySetEqual(LocationRelationPath.objects.exclude(
+        self.assertQuerySetEqual(LocationRelationPath.objects.select_related(
+            "prev_path", "adjacency", "relation"
+        ).exclude(
             Q(prev_path__isnull=True) | Q(relation__ancestor=F("prev_path__relation__ancestor"))
         ), [], msg="Found relation path segment with different relation ancestor than its prev_path")
 
-        self.assertQuerySetEqual(LocationRelationPath.objects.exclude(
+        self.assertQuerySetEqual(LocationRelationPath.objects.select_related(
+            "prev_path", "adjacency", "relation"
+        ).exclude(
             Q(prev_path__isnull=True) | Q(adjacency__parent=F("prev_path__adjacency__child"))
         ), [], msg="Found relation path segment where adjacency parent doesn't match prev_path's adjacency child")
 
@@ -39,64 +50,167 @@ class LocationHierarchyTests(TransactionTestCase):
             path_chains[path_id] = (prev_path_chain := path_chains[prev_path_id]) + (descendant, )
             relation_lookup[(ancestor, descendant)].add(prev_path_chain)
 
-        self.assertDictEqual(relation_lookup, state, msg)
+        self.assertDictEqual(state, relation_lookup, msg)
 
-    def test_single_parents_operations(self):
-        location1 = DefinedLocation.objects.create()
-        location2 = DefinedLocation.objects.create()
-        location3 = DefinedLocation.objects.create()
+    def _test_simple_add(self, add_parent_func: UpdateRelationFunc):
+        locations = DefinedLocation.objects.bulk_create([DefinedLocation() for i in range(3)])
 
-        location2.parents.add(location1)
+        add_parent_func(locations[1], locations[0])
         self.assertHierarchyState({
-            (location1.id, location2.id): {()},
-        }, msg="a→b hierarchy failed")
+            (locations[0].id, locations[1].id): {()},
+        }, msg="0→1 hierarchy failed")
 
-        location3.parents.add(location2)
+        add_parent_func(locations[2], locations[1])
         self.assertHierarchyState({
-            (location1.id, location2.id): {()},
-            (location2.id, location3.id): {()},
-            (location1.id, location3.id): {(location2.id,)},
-        }, msg="a→b→c hierarchy failed")
+            (locations[0].id, locations[1].id): {()},
+            (locations[1].id, locations[2].id): {()},
+            (locations[0].id, locations[2].id): {(locations[1].id,)},
+        }, msg="0→1→2 hierarchy failed")
 
-        location3.parents.add(location1)
+        add_parent_func(locations[2], locations[0])
         self.assertHierarchyState({
-            (location1.id, location2.id): {()},
-            (location2.id, location3.id): {()},
-            (location1.id, location3.id): {(), (location2.id,)},
-        }, msg="a→b→c + a→c hierarchy failed")
+            (locations[0].id, locations[1].id): {()},
+            (locations[1].id, locations[2].id): {()},
+            (locations[0].id, locations[2].id): {(), (locations[1].id,)},
+        }, msg="0→1→2 + 0→2 hierarchy failed")
 
-        location2.parents.remove(location1)
+    def test_simple_add_parent(self):
+        self._test_simple_add(lambda a, b: a.parents.add(b))
+
+    def test_simple_add_child(self):
+        self._test_simple_add(lambda a, b: b.children.add(a))
+
+    def _test_simple_remove(self, remove_parent_func: UpdateRelationFunc):
+        locations = DefinedLocation.objects.bulk_create([DefinedLocation() for i in range(3)])
+
+        locations[1].parents.add(locations[0])
+        locations[2].parents.add(locations[1])
+        locations[2].parents.add(locations[0])
+
+        remove_parent_func(locations[1], locations[0])
         self.assertHierarchyState({
-            (location2.id, location3.id): {()},
-            (location1.id, location3.id): {()},
+            (locations[1].id, locations[2].id): {()},
+            (locations[0].id, locations[2].id): {()},
         }, msg="hierarchy removal failed")
 
-    def test_single_childrens_operations(self):
-        location1 = DefinedLocation.objects.create()
-        location2 = DefinedLocation.objects.create()
-        location3 = DefinedLocation.objects.create()
-
-        location1.children.add(location2)
+        remove_parent_func(locations[2], locations[1])
         self.assertHierarchyState({
-            (location1.id, location2.id): {()},
-        }, msg="a→b hierarchy failed")
+            (locations[0].id, locations[2].id): {()},
+        }, msg="second hierarchy removal failed")
 
-        location2.children.add(location3)
-        self.assertHierarchyState({
-            (location1.id, location2.id): {()},
-            (location2.id, location3.id): {()},
-            (location1.id, location3.id): {(location2.id,)},
-        }, msg="a→b→c hierarchy failed")
+    def test_simple_remove_parent(self):
+        self._test_simple_remove(lambda a, b: a.parents.remove(b))
 
-        location1.children.add(location3)
-        self.assertHierarchyState({
-            (location1.id, location2.id): {()},
-            (location2.id, location3.id): {()},
-            (location1.id, location3.id): {(), (location2.id,)},
-        }, msg="a→b→c + a→c hierarchy failed")
+    def test_simple_remove_child(self):
+        self._test_simple_remove(lambda a, b: b.children.remove(a))
 
-        location1.children.remove(location2)
+    def test_simple_clear_parents(self):
+        locations = DefinedLocation.objects.bulk_create([DefinedLocation() for i in range(3)])
+        locations[1].parents.add(locations[0])
+        locations[2].parents.add(locations[1])
+        locations[2].parents.add(locations[0])
+
+        locations[2].parents.clear()
         self.assertHierarchyState({
-            (location2.id, location3.id): {()},
-            (location1.id, location3.id): {()},
-        }, msg="hierarchy removal failed")
+            (locations[0].id, locations[1].id): {()},
+        }, msg="hierarchy clear failed")
+
+    def test_simple_clear_children(self):
+        locations = DefinedLocation.objects.bulk_create([DefinedLocation() for i in range(3)])
+        locations[1].parents.add(locations[0])
+        locations[2].parents.add(locations[1])
+        locations[2].parents.add(locations[0])
+
+        locations[0].children.clear()
+        self.assertHierarchyState({
+            (locations[1].id, locations[2].id): {()},
+        }, msg="hierarchy clear failed")
+
+    def test_add_multiple_parents(self):
+        locations = DefinedLocation.objects.bulk_create([DefinedLocation() for i in range(4)])
+
+        locations[2].parents.add(locations[0], locations[1])
+        self.assertHierarchyState({
+            (locations[0].id, locations[2].id): {()},
+            (locations[1].id, locations[2].id): {()},
+        }, msg="adding two parents once failed")
+
+        locations[3].parents.add(locations[1], locations[2])
+        self.assertHierarchyState({
+            (locations[0].id, locations[2].id): {()},
+            (locations[1].id, locations[2].id): {()},
+            (locations[1].id, locations[3].id): {(), (locations[2].id, )},
+            (locations[2].id, locations[3].id): {()},
+            (locations[0].id, locations[3].id): {(locations[2].id, )},
+        }, msg="adding two more parents failed")
+
+    def test_add_multiple_children(self):
+        locations = DefinedLocation.objects.bulk_create([DefinedLocation() for i in range(4)])
+
+        locations[0].children.add(locations[1], locations[2])
+        self.assertHierarchyState({
+            (locations[0].id, locations[1].id): {()},
+            (locations[0].id, locations[2].id): {()},
+        }, msg="adding two children once failed")
+
+        locations[1].children.add(locations[2], locations[3])
+        self.assertHierarchyState({
+            (locations[0].id, locations[1].id): {()},
+            (locations[0].id, locations[2].id): {(), (locations[1].id, )},
+            (locations[1].id, locations[2].id): {()},
+            (locations[1].id, locations[3].id): {()},
+            (locations[0].id, locations[3].id): {(locations[1].id, )},
+        }, msg="adding two more children failed")
+
+    def _test_add_downwards_tree(self, add_parent_func: UpdateRelationFunc):
+        locations = DefinedLocation.objects.bulk_create([DefinedLocation() for i in range(4)])
+
+        add_parent_func(locations[1], locations[0])
+        add_parent_func(locations[2], locations[1])
+        add_parent_func(locations[2], locations[0])
+        add_parent_func(locations[0], locations[3])
+
+        self.assertHierarchyState({
+            (locations[0].id, locations[1].id): {()},
+            (locations[1].id, locations[2].id): {()},
+            (locations[0].id, locations[2].id): {(), (locations[1].id,)},
+            (locations[3].id, locations[0].id): {()},
+            (locations[3].id, locations[1].id): {(locations[0].id,)},
+            (locations[3].id, locations[2].id): {(locations[0].id,), (locations[0].id, locations[1].id,)},
+        }, msg="3→0→1→2 + 0→2 hierarchy failed")
+
+    def test_add_downwards_tree_by_parent(self):
+        self._test_add_downwards_tree(lambda a, b: a.parents.add(b))
+
+    def test_add_downwards_tree_by_child(self):
+        self._test_add_downwards_tree(lambda a, b: b.children.add(a))
+
+    def _test_add_upwards_tree(self, add_parent_func: UpdateRelationFunc):
+        locations = DefinedLocation.objects.bulk_create([DefinedLocation() for i in range(4)])
+
+        add_parent_func(locations[1], locations[0])
+        add_parent_func(locations[2], locations[1])
+        add_parent_func(locations[2], locations[0])
+        add_parent_func(locations[3], locations[2])
+
+        self.assertHierarchyState({
+            (locations[0].id, locations[1].id): {()},
+            (locations[1].id, locations[2].id): {()},
+            (locations[0].id, locations[2].id): {(), (locations[1].id,)},
+            (locations[2].id, locations[3].id): {()},
+            (locations[1].id, locations[3].id): {(locations[2].id,)},
+            (locations[0].id, locations[3].id): {(locations[2].id,), (locations[1].id, locations[2].id,)},
+        }, msg="0→1→2→3 + 0→2 hierarchy failed")
+
+    def test_add_upwards_tree_by_parent(self):
+        self._test_add_upwards_tree(lambda a, b: a.parents.add(b))
+
+    def test_add_upwards_tree_by_child(self):
+        self._test_add_upwards_tree(lambda a, b: b.children.add(a))
+
+    def test_circular_fails(self):
+        locations = DefinedLocation.objects.bulk_create([DefinedLocation() for i in range(3)])
+        locations[0].children.add(locations[1])
+        locations[1].children.add(locations[2])
+        with self.assertRaises(CircularyHierarchyError):
+            locations[2].children.add(locations[0])
