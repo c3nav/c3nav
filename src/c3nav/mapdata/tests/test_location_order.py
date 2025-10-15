ad452febae3d4ae8549a92d371a7e20bc801a335
Laura Klünder <laura@codingcatgirl.de>
2025-10-15 14:12:18 +0100

    make location order tests more reusable

--- src/c3nav/mapdata/tests/test_location_order.py
+++ src/c3nav/mapdata/tests/test_location_order.py
@@ -1,3 +1,5 @@
+from typing import NamedTuple
+
 from django.test.testcases import TransactionTestCase
 
 from c3nav.mapdata import process
@@ -5,62 +7,78 @@ from c3nav.mapdata.models import AccessRestriction, Theme
 from c3nav.mapdata.models.locations import LocationTag, LabelSettings
 
 
-class LocationInheritanceTests(TransactionTestCase):
+class LocationTagDirectionOrders(NamedTuple):
+    breadth_first_order: tuple[LocationTag, ...]
+    depth_first_pre_order: tuple[LocationTag, ...]
+    depth_first_post_order: tuple[LocationTag, ...]
+
+
+class ExpectedLocationTagOrderResult(NamedTuple):
+    upwards: LocationTagDirectionOrders
+    downwards: LocationTagDirectionOrders
+    ancestors: dict[LocationTag, LocationTagDirectionOrders]
+    descendants: dict[LocationTag, LocationTagDirectionOrders]
+
+
+class LocationTagOrderTests(TransactionTestCase):
     def setUp(self):
         LocationTag.objects.all().delete()
         self.access_restriction = AccessRestriction.objects.create(titles={"en": "Restriction 1"})
         self.label_settings = LabelSettings.objects.create()
         self.theme = Theme.objects.create(description="MyTheme")
 
-    def _create_tags(self, priorities: tuple[int, ...]):
-        return LocationTag.objects.bulk_create([LocationTag(priority=priority) for priority in priorities])  # pragma: no branch
+    def _create_tag(self, name, priority: int) -> LocationTag:
+        return LocationTag.objects.create(priority=priority, titles={"en": name})
 
-    def _recalculate(self):
+    def _create_tags(self, name, priorities: tuple[int, ...]) -> list[LocationTag]:
+        return LocationTag.objects.bulk_create((
+            LocationTag(priority=priority, titles={"en": f"{name}{i}"})
+            for i, priority in enumerate(priorities, start=1)
+        ))
+
+    def assertLocationOrder(self, expected: ExpectedLocationTagOrderResult):
         process.process_location_tag_relations()
+        for direction in ("downwards", "upwards"):
+            for order_label in ("breadth_first_order", "depth_first_pre_order", "depth_first_post_order"):
+                self.assertQuerySetEqual(
+                    LocationTag.objects.order_by(f"effective_{direction}_{order_label}"),
+                    getattr(getattr(expected, direction), order_label),
+                    msg=f"{direction} {order_label.replace("_", " ")} doesn't match"
+                )
+
+        for relation, direction in (("ancestors", "upwards_relations__effective_downwards_"),
+                                    ("descendants", "downwards_relations__effective_upwards_")):
+            for tag, orders in getattr(expected, relation).items():
+                for order_label in ("breadth_first_order", "depth_first_pre_order", "depth_first_post_order"):
+                    self.assertQuerySetEqual(
+                        getattr(tag, f"calculated_{relation}").order_by(f'{direction}_{order_label}'),
+                        getattr(orders, order_label),
+                        msg=f"{tag} {relation} {order_label.replace("_", " ")} doesn't match"
+                    )
 
     def test_triple_tree_and_two_singles(self):
-        parent = LocationTag.objects.create(priority=5, titles={"en": "parent"}) # 8
-        children = [
-            LocationTag.objects.create(priority=3, titles={"en": "child1"}), # 9
-            LocationTag.objects.create(priority=1, titles={"en": "child2"}), # 10
-            LocationTag.objects.create(priority=2, titles={"en": "child3"}), # 11
-        ]
-        singles = [
-            LocationTag.objects.create(priority=3, titles={"en": "single1"}), # 12 → 2
-            LocationTag.objects.create(priority=6, titles={"en": "single2"}), # 13 → 0
-        ]
+        """
+                parent(5)          single1(3)      single(6)
+               /    |    \
+        child(3) child(1) child(2)
+        """
+        parent = self._create_tag("parent", 5)
+        children = self._create_tags("child", (3, 1, 2))
+        singles = self._create_tags("single", (3, 6))
         for child in children:
             child.parents.add(parent)
-        self._recalculate()
 
-        self.assertQuerySetEqual(
-            LocationTag.objects.order_by("effective_downwards_breadth_first_order"),
-            (singles[1], parent, singles[0], children[0], children[2], children[1]),
-            msg="downwards breadth first order doesnt match"
-        )
-        print(LocationTag.objects.values_list("pk", "effective_downwards_depth_first_pre_order"))
-        self.assertQuerySetEqual(
-            LocationTag.objects.order_by("effective_downwards_depth_first_pre_order"),
-            (singles[1], parent, children[0], children[2], children[1], singles[0]),
-            msg="downwards depth first pre-order doesnt match"
-        )
-        self.assertQuerySetEqual(
-            LocationTag.objects.order_by("effective_downwards_depth_first_post_order"),
-            (singles[1], children[0], children[2], children[1], parent, singles[0]),
-            msg="downwards depth first post-order doesnt match"
-        )
-        self.assertQuerySetEqual(
-            LocationTag.objects.order_by("effective_upwards_breadth_first_order"),
-            (singles[1], children[0], singles[0], children[2], children[1], parent),
-            msg="upwards breadth first order doesnt match"
-        )
-        self.assertQuerySetEqual(
-            LocationTag.objects.order_by("effective_upwards_depth_first_pre_order"),
-            (singles[1], children[0], parent, singles[0], children[2], children[1]),
-            msg="upwards depth first pre-order doesnt match"
-        )
-        self.assertQuerySetEqual(
-            LocationTag.objects.order_by("effective_upwards_depth_first_post_order"),
-            (singles[1], parent, children[0], singles[0], children[2], children[1]),
-            msg="upwards depth first post-order doesnt match"
-        )
+        self.assertLocationOrder(ExpectedLocationTagOrderResult(
+            downwards=LocationTagDirectionOrders(
+                breadth_first_order=(singles[1], parent, singles[0], children[0], children[2], children[1]),
+                depth_first_pre_order=(singles[1], parent, children[0], children[2], children[1], singles[0]),
+                depth_first_post_order=(singles[1], children[0], children[2], children[1], parent, singles[0]),
+            ),
+            upwards=LocationTagDirectionOrders(
+                breadth_first_order=(singles[1], children[0], singles[0], children[2], children[1], parent),
+                depth_first_pre_order=(singles[1], children[0], parent, singles[0], children[2], children[1]),
+                depth_first_post_order=(singles[1], parent, children[0], singles[0], children[2], children[1]),
+            ),
+            ancestors={},
+            descendants={},
+        ))
