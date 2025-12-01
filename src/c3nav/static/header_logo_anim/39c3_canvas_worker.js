3366509c08f0cd3ef0d46e77d3269f0dd53cc3c4
Jenny Paxian <github@jenny.earth>
2025-12-01 14:02:33 +0100

    Off-Scale font weighting support for canvas worker Font loading in sandbox example
    Scale CanvasWorker based on Device DPI

--- src/c3nav/static/header_logo_anim/39c3_canvas_worker.js
+++ src/c3nav/static/header_logo_anim/39c3_canvas_worker.js
@@ -1,38 +1,55 @@
 // Worker for 39C3 animation canvas
 let canvas, ctx, frame;
-
-// Listen for the canvas from c3nav main thread
-self.onmessage = (e) => {
-    if (e.data.canvas !== undefined) {
-      canvas = e.data.canvas;
-      ctx = canvas.getContext("2d");
-      setup();
-    }
-
-    if (e.data.pause)
-      cancelAnimationFrame(frame);
-    else
-      frame = requestAnimationFrame(draw);
-};
+let dpr = 1.0;
 
 // The text and x y coords within canvas
 const text = "C3NAV";
-const baseX = 0;
-const baseY = 49;
 
 // min max weight and how many steps/how smooth
-const minWeight = 100;
-const maxWeight = 900;
+const minWeight = 10;
+const maxWeight = 100;
 const weightStep = 10;
 
 // which font and size
-const fontSize = 32;
-const fontFamily = "Inter, sans-serif";
+const fontSize = 30;
+const fontFamily = 'KarioDuplexVar';
+const fontFallback = 'sans-serif';
+const fontURL = "url('/static/header_logo_anim/Kario39C3VarWEB-Roman.woff2')";
+const fontStyle = {
+    style: "normal",
+    weight: "10 100"
+};
+const letterSpacing = 0;
 
 // time between frames
 const speed = 0.005;
 // time between letters (phase)
-const phaseDist = 0.7;
+const phaseDist = 0.7
+
+const font = new FontFace(fontFamily, fontURL, fontStyle);
+
+// Listen for the canvas from c3nav main thread
+self.onmessage = (e) => {
+    if (e.data.canvas !== undefined) {
+        canvas = e.data.canvas;
+        dpr = e.data.dpr;
+        ctx = canvas.getContext("2d");
+
+        font.load().then(() => {
+            // Setup with loaded font
+            self.fonts.add(font);
+            setup();
+        }, (err) => {
+            // Setup anyway, it will fallback unless font is installed on system
+            setup();
+        });
+    }
+
+    if (e.data.pause)
+        cancelAnimationFrame(frame);
+    else
+        frame = requestAnimationFrame(draw);
+};
 
 const charWidths = {};
 const charCache = {};
@@ -43,9 +60,10 @@ function setup() {
         charCache[weight] = {};
         charWidths[weight] = {};
         [...text].forEach(ch => {
-            const tmpCanvas = new OffscreenCanvas(100, 120);
+            const tmpCanvas = new OffscreenCanvas(fontSize*dpr, fontSize*dpr);
             const tmpCtx = tmpCanvas.getContext("2d");
-            tmpCtx.font = `${weight} ${fontSize}px ${fontFamily}`;
+            tmpCtx.fontKerning = "none";
+            tmpCtx.font = `${weight} ${fontSize*dpr}px ${fontFamily}, ${fontFallback}`;
             tmpCtx.fillStyle = "white";
             tmpCtx.textBaseline = "top";
             tmpCtx.fillText(ch, 0, 0);
@@ -56,24 +74,27 @@ function setup() {
 }
 
 function draw(t) {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    // Check if glyphs are rendered yet
+    if (Object.keys(charCache).length !== 0) {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    let x = baseX;
-    for (let i = 0; i < text.length; i++) {
-        const phase = t * speed + i * phaseDist;
-        // smooth out the phase by approximating it to a sine wave
-        const sine = (Math.sin(phase) + 1) / 2;
+        let x = 0;
+        for (let i = 0; i < text.length; i++) {
+            const phase = t * speed + i * phaseDist;
+            // smooth out the phase by approximating it to a sine wave
+            const sine = (Math.sin(phase) + 1) / 2;
 
-        // get prerendered canvas and width
-        let weight = minWeight + sine * (maxWeight - minWeight);
-        weight = Math.round(weight / weightStep) * weightStep;
-        weight = Math.min(Math.max(weight, minWeight), maxWeight);
+            // get prerendered canvas and width
+            let weight = minWeight + sine * (maxWeight - minWeight);
+            weight = Math.round(weight / weightStep) * weightStep;
+            weight = Math.min(Math.max(weight, minWeight), maxWeight);
 
-        const charCanvas = charCache[weight][text[i]];
-        ctx.drawImage(charCanvas, x, baseY - fontSize);
+            const charCanvas = charCache[weight][text[i]];
+            // Position rendered char at width offset and centered height
+            ctx.drawImage(charCanvas, x, (canvas.height - (fontSize * dpr / 2)) - fontSize * dpr);
 
-        x += charWidths[weight][text[i]] + 5;
+            x += charWidths[weight][text[i]] + letterSpacing;
+        }
     }
-
     frame = requestAnimationFrame(draw);
 }
