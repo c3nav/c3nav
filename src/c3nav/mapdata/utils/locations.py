f00b57b62b992c727e61062d1121fc917eaf6e9c
Laura Klünder <laura@codingcatgirl.de>
2025-12-27 12:44:21 +0100

    fix distancelocation feature

--- src/c3nav/mapdata/utils/locations.py
+++ src/c3nav/mapdata/utils/locations.py
@@ -14,6 +14,7 @@ from django.utils.functional import cached_property
 from django.utils.text import format_lazy
 from django.utils.translation import get_language
 from django.utils.translation import gettext_lazy as _
+from shapely import Point
 from shapely.ops import unary_union
 
 from c3nav.mapdata.grid import grid
@@ -477,22 +478,30 @@ class DistanceLocationFeature:
                 cls.location = LocationSlug.objects.get(
                     pk=settings.DISTANCE_FROM_LOCATION).get_child()
                 point = cls.location.point
+                if not isinstance(point, Point):
+                    point = Point(point[1:])
             except:
                 return
 
             cls.xyz = (
-                point.x, point.y,
+                *(point[1:] if isinstance(point, tuple) else (point.x, point.y)),
                 router.spaces[
-                    cls.location.pk if isinstance(cls.location, Space) else cls.location.space_id
+                    cls.location.pk if isinstance(cls.location, Space) else (
+                        location.space_id if hasattr(location, "space_id") else location.space.id
+                    )
                 ].altitudearea_for_point(point).get_altitude(point)
             )
 
         try:
             point = location.point
+            if not isinstance(point, Point):
+                point = Point(point[1:])
             other_xyz = (
-                point.x, point.y,
+                *(point[1:] if isinstance(point, tuple) else (point.x, point.y)),
                 router.spaces[
-                    location.pk if isinstance(location, Space) else cls.location.space_id
+                    location.pk if isinstance(location, Space) else (
+                        location.space_id if hasattr(location, "space_id") else location.space.id
+                    )
                 ].altitudearea_for_point(point).get_altitude(point)
             )
         except:
