e0689eb7b3899dc24f55aae53477e161c64437ae
Laura Klünder <laura@codingcatgirl.de>
2026-03-24 18:59:55 +0100

    add server code

--- src/c3nav/mapdata/management/commands/staticarchive.py
+++ src/c3nav/mapdata/management/commands/staticarchive.py
@@ -2,14 +2,51 @@ import argparse
 from pathlib import Path
 from tempfile import TemporaryDirectory
 
-from django.conf import settings
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 from django.utils.translation import gettext_lazy as _
 from django.utils.translation import ngettext_lazy
 
-from c3nav.mapdata.models import AccessRestriction, Source
-from c3nav.mapdata.render.engines import get_engine
-from c3nav.mapdata.render.renderer import MapRenderer
+from c3nav.mapdata.models import AccessRestriction
+
+SERVER_CODE = """
+import http.server
+import socketserver
+
+from http import HTTPStatus
+
+PORT = 8000
+
+class StaticArchiveHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def send_error(self, code, message=None, explain=None):
+        if code == 404:
+            if self.path.startswith("/map/") and (self.path.endswith(".webp") or self.path.endswith(".png")):
+                self.send_response(HTTPStatus.SEE_OTHER)
+                self.send_header("Location", f"/map/blank/{self.path.split('/')[-1]}")
+                self.send_header("Content-Length", "0")
+                self.end_headers()
+                return
+        
+        return super().send_error(code, message, explain)
+      
+    def translate_path(self, path):
+        if path.startswith('/o/') or path.startswith('/d/'):
+            path = f"/l/{path[3:]}"
+        elif path.startswith('/r/'):
+            split = path[3:].split("/")
+            if len(split) > 3:
+                path = f"/l/{split[3]}"
+        result = super().translate_path(path).split("@")[0]
+        if result.endswith("/details/"):
+            result = result.removesuffix('details/')
+        return result
+
+
+with socketserver.TCPServer(("", PORT), StaticArchiveHTTPRequestHandler) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()
+    
+    {"logged_in":false,"allow_editor":false,"allow_control_panel":false,"mesh_control":false,"has_positions":false,"title":"Archive","subtitle":null,"permissions":[],"overlays":[],"quests":{}}
+"""
 
 
 class Command(BaseCommand):
@@ -58,3 +95,6 @@ class Command(BaseCommand):
         from c3nav.mapdata.archive import static_archive as mapdata_static_archive
         site_static_archive(output_dir=output_dir, permissions=permissions, png=include_png)
         mapdata_static_archive(output_dir=output_dir, permissions=permissions, png=include_png)
+
+        with (output_dir / "server.p").open("w") as f:
+            f.write(SERVER_CODE)
