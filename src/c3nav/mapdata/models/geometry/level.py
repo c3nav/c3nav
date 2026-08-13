93f403929632125255211cbaba2264d9be0e684b
Florian Maurer <maurer@fh-aachen.de>
2026-08-13 11:18:35 +0000

    feat(mapdata): add name to building

--- src/c3nav/mapdata/models/geometry/level.py
+++ src/c3nav/mapdata/models/geometry/level.py
@@ -114,11 +114,19 @@ class Building(LevelGeometryMixin, models.Model):
     The outline of a building on a specific level
     """
     geometry = GeometryField('polygon')
+    name = models.CharField(_('Name'), max_length=50, blank=True, null=True)
 
     class Meta:
         verbose_name = _('Building')
         verbose_name_plural = _('Buildings')
         default_related_name = 'buildings'
+
+    @property
+    def title(self):
+        return self.name or super().title
+
+    def __str__(self):
+        return self.title
 
 
 class Space(LevelGeometryMixin, SpecificLocation, models.Model):
